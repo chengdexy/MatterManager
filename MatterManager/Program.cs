93b9b3cc67cbc8d89d7dedea9f2fc209ea6b938e
单位电脑 <Administrator@MS-20161020PKJU>
2017-01-24 12:31:22 +0800

    解决了以clickonce启动时,无法获取管理员权限的问题

--- MatterManager/Program.cs
+++ MatterManager/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,6 +11,7 @@ namespace MatterManager
 {
     static class Program
     {
+        /*
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
@@ -17,6 +21,37 @@ namespace MatterManager
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
+        }
+        */
+        [STAThread]
+        static void Main()
+        {
+            Action run = () =>
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frmMain());
+            };
+            WindowsIdentity wi = WindowsIdentity.GetCurrent();
+            bool runAsAdmin = wi != null && new WindowsPrincipal(wi).IsInRole(WindowsBuiltInRole.Administrator);
+            if (!runAsAdmin)
+            {
+                try
+                {
+                    //不可能以管理员方式直接启动一个 ClickOnce 部署的应用程序，所以尝试以管理员方式启动一个新的进程
+                    Process.Start(new ProcessStartInfo(Assembly.GetExecutingAssembly().CodeBase) { UseShellExecute = true, Verb = "runas" });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(string.Format("以管理员方式启动失败，将尝试以普通方式启动！{0}{1}", Environment.NewLine, ex), "出错啦！", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    run();//以管理员方式启动失败，则尝试普通方式启动
+                }
+                Application.Exit();
+            }
+            else
+            {
+                run();
+            }
         }
     }
 }
